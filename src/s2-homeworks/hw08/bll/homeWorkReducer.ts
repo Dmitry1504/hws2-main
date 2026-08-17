44dece83a23756c20e4dc28cda37e91e4517eda4
Dmitry1504 <xxxdemongtxxx@gmail.com>
2026-08-17 19:33:22 +0300

    Commit

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const sorted  = [...state].sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name.localeCompare(b.name)
+                } else {
+                    return b.name.localeCompare(a.name)
+                }
+            })
+            return sorted
         }
-        case 'check': {
 
-            return state // need to fix
+        case 'check': {
+            return state.filter(user => user.age >= action.payload)
         }
         default:
             return state
